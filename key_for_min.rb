59bc2ca1a64bb0a0aae0bb454f2ff1b702e17a5f
Ingrid sura <sura.ingrid@gmail.com>
2019-11-19 01:31:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,6 @@
 snacks = {:hotcheetos => 20, :hotfries => 10, :hotnuts => 3}
 def key_for_min_value(snacks)
   snacks.collect do |hotfood,bags|
-    food
+    food < 3
 key_for_min_value.collect
 end

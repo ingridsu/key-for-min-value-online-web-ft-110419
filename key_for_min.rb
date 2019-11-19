6939500aef1db35434d0346cef2ae38b36bdfe46
Ingrid sura <sura.ingrid@gmail.com>
2019-11-19 01:26:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-snacks = {:hotcheetos => spicy, :brownies => sweet, :nuts => salty}
+snacks = {:hotcheetos => 20, :hotfries => 10, :hotnuts => 3}
 def key_for_min_value(snacks)
   snacks.collect do |food,flavor|
     food
